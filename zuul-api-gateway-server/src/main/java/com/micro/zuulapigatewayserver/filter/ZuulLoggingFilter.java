f5e4d9d5cce5362bda7c1d778e1fce311c32fa2b
Jithin Prasad R <jithin.prasad.r@gmail.com>
2018-12-26 17:35:22 +0530

    Zipkin and kafka

---
+++ zuul-api-gateway-server/src/main/java/com/micro/zuulapigatewayserver/filter/ZuulLoggingFilter.java
@@ -0,0 +1,41 @@
+package com.micro.zuulapigatewayserver.filter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class ZuulLoggingFilter extends ZuulFilter {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Override
+	public Object run() throws ZuulException {
+		HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+		logger.info(request.getRequestURI()); 
+		
+		return null;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+}
