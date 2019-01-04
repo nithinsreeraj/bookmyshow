c6f1aa55ec244c162ee45e3dfa8aedbf7d346661
Nithin Sreeraj H R <NithinR@namindia.com>
2019-01-04 10:25:44 +0530

    Fixed conflict errors

--- premiere-catalogue-service/src/main/java/com/microservices/bookmyshow/theatrecatalogueservice/repo/TheatreRepository.java
+++ premiere-catalogue-service/src/main/java/com/microservices/bookmyshow/theatrecatalogueservice/repo/TheatreRepository.java
@@ -1,9 +1,5 @@
 package com.microservices.bookmyshow.theatrecatalogueservice.repo;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 518e4a71ee41b5da6968fb07ab8311eedaa26265
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.microservices.bookmyshow.theatrecatalogueservice.entity.Theatre;
@@ -11,11 +7,5 @@ import com.microservices.bookmyshow.theatrecatalogueservice.entity.Theatre;
 
 public interface TheatreRepository extends JpaRepository<Theatre, Integer>{
 	
-<<<<<<< HEAD
-	
-=======
-	//ArrayList<Theatre> findByMovieIdAndCityIdAndDate(int movieId, int cityId, java.time.LocalDateTime showDateTime );
-
->>>>>>> 518e4a71ee41b5da6968fb07ab8311eedaa26265
 
 }
--- premiere-catalogue-service/src/main/resources/data.sql
+++ premiere-catalogue-service/src/main/resources/data.sql
@@ -1,8 +1,8 @@
 
-insert into Theatre (theatre_id, theatre_name, city_id, movie_id) values (1, 'Aries Plex', 1,1000),
+insert into theatre (theatre_id, theatre_name, city_id, movie_id) values (1, 'Aries Plex', 1,1000),
 (2, 'Mall of travancore' , 1,1001) , (3, 'Centrum mall', 1, 1002);
 
-insert into Show (theatre_id, show_time, total_seats, available_seats) values (1, '2018-12-26 11:30:00.0000', 100, 100 ),
+insert into show (theatre_id, show_date_time, total_seats, available_seats) values (1, '2018-12-26 11:30:00.0000', 100, 100 ),
 (1, '2018-12-26 16:30:00.0000', 100, 100 ),
 (2, '2018-12-26 11:30:00.0000', 100, 100 ),
 (2, '2018-12-26 16:30:00.0000', 100, 100 );
