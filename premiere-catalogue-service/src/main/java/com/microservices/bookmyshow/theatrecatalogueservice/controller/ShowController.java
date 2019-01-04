be1f0aae9f4e15fefb678b716786a63934a14790
Nithin Sreeraj H R <NithinR@namindia.com>
2019-01-04 11:43:19 +0530

    Added two more get mappings

--- premiere-catalogue-service/src/main/java/com/microservices/bookmyshow/theatrecatalogueservice/controller/ShowController.java
+++ premiere-catalogue-service/src/main/java/com/microservices/bookmyshow/theatrecatalogueservice/controller/ShowController.java
@@ -1,20 +1,26 @@
 package com.microservices.bookmyshow.theatrecatalogueservice.controller;
 
+import java.text.ParseException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.microservices.bookmyshow.theatrecatalogueservice.dto.BookingDetailsDTO;
 import com.microservices.bookmyshow.theatrecatalogueservice.dto.SeatInfoDTO;
-import com.microservices.bookmyshow.theatrecatalogueservice.dto.ShowFilterDTO;
 import com.microservices.bookmyshow.theatrecatalogueservice.entity.Seat;
+import com.microservices.bookmyshow.theatrecatalogueservice.entity.Show;
 import com.microservices.bookmyshow.theatrecatalogueservice.entity.Theatre;
 import com.microservices.bookmyshow.theatrecatalogueservice.repo.SeatRepository;
+import com.microservices.bookmyshow.theatrecatalogueservice.repo.ShowRepository;
 import com.microservices.bookmyshow.theatrecatalogueservice.repo.TheatreRepository;
 
 
@@ -22,27 +28,34 @@ import com.microservices.bookmyshow.theatrecatalogueservice.repo.TheatreReposito
 public class ShowController {
 
 	@Autowired TheatreRepository theatreRepo;
-	@Autowired EntityManager manager;
+	@Autowired ShowRepository showRepo;
 
     @Autowired
     private SeatRepository seatRepo;
 
-	@PostMapping("/shows")
-	public List<Theatre> getShows(@RequestBody ShowFilterDTO filterDTO){
+	@GetMapping("/theatres")
+	public @ResponseBody List<Theatre> getTheatres(@RequestParam("cityId") int cityId, @RequestParam("movieId") int movieId) throws ParseException{
 		
-		StringBuffer buf = new StringBuffer ();
-		buf.append(" select theatreId, theatreName, cityId, movieId from Theatre t ");
-//		buf.append(" inner join show sh on sh.theatre_id = t.theatre_id");
-//		buf.append(" where 1=1");
-//		buf.append(" and t.movie_id = " + filterDTO.getMovieId());
-//		buf.append(" and t.city_id = " + filterDTO.getCityId());
-//		buf.append(" and sh.show_date_time = " + filterDTO.getShowDateTime());
+		if(cityId==0 || movieId == 0) {
+			throw new ParseException(" Missing query parameters",0);
+		}
 		
-		List<Theatre> result = manager.createQuery(buf.toString()).getResultList();
-		
+		List<Theatre> result = theatreRepo.findByCityIdAndMovieId(cityId, movieId);
 		
 		return result;
-
+		
+	}
+	
+	@GetMapping("/theatres/{theatreId}/shows")
+	public  @ResponseBody List<Show> getShows(@PathVariable("theatreId") int theatreId) throws ParseException{
+		
+		if(theatreId==0) {
+			throw new ParseException(" Missing query parameters",0);
+		}
+		
+		List<Show> shows = showRepo.findByTheatreId(theatreId);
+		
+		return shows;
 		
 	}
 
--- premiere-catalogue-service/src/main/java/com/microservices/bookmyshow/theatrecatalogueservice/repo/ShowRepository.java
+++ premiere-catalogue-service/src/main/java/com/microservices/bookmyshow/theatrecatalogueservice/repo/ShowRepository.java
@@ -7,11 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.microservices.bookmyshow.theatrecatalogueservice.entity.Show;
-import com.microservices.bookmyshow.theatrecatalogueservice.entity.Theatre;
 
+public interface ShowRepository extends JpaRepository<Show, Long>{
+	
+	@Query(" select showId, theatreId, showDateTime, totalSeats, availableSeats from Show s where s.theatreId = :theatreId")
+	public List<Show> getShowList(@Param("theatreId") int theatreId);
 
-public interface TheatreRepository extends JpaRepository<Theatre, Integer>{
-	
-	public List<Theatre> findByCityIdAndMovieId(int cityId, int movieId);
-	
+	public List<Show> findByTheatreId(int theatreId);
+
 }
